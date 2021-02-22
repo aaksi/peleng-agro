1a57e5338ffa3b57beb289687d10ff9d7033ad6a
aaksi <yyff91@mail.ru>
2021-02-22 22:02:20 +0300

    added header, top

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,40 @@
 $(function(){
   
+    $('.menu__btn').on('click', function(){
+        $(this).toggleClass('menu__btn--active')
+        $('.menu__list-wrapper').slideToggle(700);
+    });
 
+    // $('.top__slider').slick();
+
+    
+    var $slider = $('.top__slider');
+    
+    if ($slider.length) {
+        var currentSlide;
+        var slidesCount;
+        var sliderCounter = document.createElement('div');
+        sliderCounter.classList.add('slider__counter');
+        
+        var updateSliderCounter = function(slick, currentIndex) {
+            currentSlide = slick.slickCurrentSlide() + 1;
+            slidesCount = slick.slideCount;
+            $(sliderCounter).text(currentSlide + '/' +slidesCount)
+        };
+    
+        $slider.on('init', function(event, slick) {
+            $slider.append(sliderCounter);
+            updateSliderCounter(slick);
+        });
+    
+        $slider.on('afterChange', function(event, slick, currentSlide) {
+            updateSliderCounter(slick, currentSlide);
+        });
+    
+        $slider.slick({
+            prevArrow: '<button type="button" class="slider-btn slider-btn--left">Next</button>',
+            nextArrow: '<button type="button" class="slider-btn slider-btn--right">Next</button>',
+            
+        });
+    }
 });
